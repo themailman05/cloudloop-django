70058686846079d7639c7c16a13b98b8813f5831
Liam Sargent <liam@ziptech.io>
2020-04-25 04:56:00 -0400

    Add looper models and migrations.

---
+++ cloudloop/looper/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+from cloudloop import helper
+
+
+class Session(models.Model):
+    """Session for looping
+
+    - Users join a session when added to clients
+    """
+    hash_id = models.CharField(max_length=32, default=helper.create_hash, unique=True)
+    title = models.CharField(max_length=64)
+    clients = models.ManyToManyField('userauth.User', blank=True)
+    loops = models.ForeignKey('looper.Loop', null=True, blank=True, on_delete=models.SET_NULL)
+
+
+class Loop(models.Model):
+    """Session Loop
+    """
+    hash_id = models.CharField(max_length=32, default=helper.create_hash, unique=True)
+    timestamp = models.IntegerField(default=helper.time_stamp)
+    beats = models.IntegerField()
+    sample_rate = models.IntegerField(choices=[(44100,'44.1Khz'), (48000, '48Khz'), (96000, '96Khz')])
+    bit_depth = models.IntegerField(choices=[(24,'24 bit'), (16, '16 bit')])
+    channels = models.IntegerField(choices=[(1,'mono'),(2,'stereo')])
+    wav_link = models.URLField(unique=True)
+    creator = models.ForeignKey('userauth.User', on_delete=models.SET_NULL, null=True)
+    sessions = models.ManyToManyField(Session)
+
+# Create your models here.
