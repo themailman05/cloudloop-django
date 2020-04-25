7452a1c6f830a6928eea10154a1e846217d8c99c
Liam Sargent <liam@ziptech.io>
2020-04-25 06:15:43 +0000

    User Auth

--- cloudloop/cloudloop/settings.py
+++ cloudloop/cloudloop/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'userauth'
 ]
 
 MIDDLEWARE = [
@@ -103,6 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'userauth.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
